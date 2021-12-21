e90b1b2ab4566acf79d2ce223dd2471e2a084180
Ian Ker-Seymer <i.kerseymer@gmail.com>
2021-12-21 00:12:07 -0500

    Add include flags to link args

--- build.rs
+++ build.rs
@@ -6,62 +6,69 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn rbconfig(key: &str) -> String {
-    let ruby = env::var_os("RUBY").unwrap_or(OsString::from("ruby"));
+  let ruby = env::var_os("RUBY").unwrap_or(OsString::from("ruby"));
 
-    let config = Command::new(ruby)
-        .arg("-e")
-        .arg(format!("print RbConfig::CONFIG['{}']", key))
-        .output()
-        .unwrap_or_else(|e| panic!("ruby not found: {}", e));
+  let config = Command::new(ruby)
+    .arg("-e")
+    .arg(format!("print RbConfig::CONFIG['{}']", key))
+    .output()
+    .unwrap_or_else(|e| panic!("ruby not found: {}", e));
 
-    String::from_utf8(config.stdout).expect("RbConfig value not UTF-8!")
+  String::from_utf8(config.stdout).expect("RbConfig value not UTF-8!")
 }
 
 fn main() {
-    let ruby_libs = rbconfig("LIBS");
+  let ruby_libs = rbconfig("LIBS");
 
-    if !ruby_libs.trim().is_empty() {
-        println!("cargo:rustc-link-arg={}", ruby_libs);
+  if !ruby_libs.trim().is_empty() {
+    println!("cargo:rustc-link-arg={}", ruby_libs);
+  }
+
+  let libruby_arg = rbconfig("LIBRUBYARG");
+
+  if !libruby_arg.trim().is_empty() {
+    println!("cargo:rustc-link-arg={}", libruby_arg);
+  }
+
+  rbconfig("DLDFLAGS").split(' ').for_each(|arg| {
+    if !arg.trim().is_empty() {
+      println!("cargo:rustc-link-arg={}", arg);
     }
+  });
 
-    let libruby_arg = rbconfig("LIBRUBYARG");
+  println!(
+    "cargo:rustc-link-arg={}",
+    format!("-I{}", rbconfig("rubyhdrdir"))
+  );
+  println!("cargo:rustc-link-arg={}", format!("-I{}", rbconfig("rubyarchhdrdir")));
+  println!("cargo:rerun-if-changed=wrapper.h");
+  println!("cargo:rerun-if-changed=build.rs");
+  println!("cargo:rerun-if-changed=wrapper.h");
 
-    if !libruby_arg.trim().is_empty() {
-        println!("cargo:rustc-link-arg={}", libruby_arg);
-    }
+  let bindings = bindgen::Builder::default()
+    .header("wrapper.h")
+    .use_core()
+    .ctypes_prefix("::libc")
+    .blocklist_item(".*mjit.*")
+    .blocklist_item("^FP_.*")
+    .allowlist_file(".*/ruby/[^/]+\\.h")
+    .rustified_enum("*")
+    .new_type_alias_deref("VALUE")
+    .default_alias_style(bindgen::AliasVariation::NewType)
+    .derive_eq(true)
+    .derive_debug(true)
+    .clang_args(&[
+      format!("-I{}", rbconfig("rubyhdrdir")),
+      format!("-I{}", rbconfig("rubyarchhdrdir")),
+      "-fdeclspec".to_string(),
+    ])
+    .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+    .generate()
+    .expect("Unable to generate bindings");
 
-    rbconfig("DLDFLAGS").split(' ').for_each(|arg| {
-        println!("cargo:rustc-link-arg={}", arg);
-    });
-
-    println!("cargo:rerun-if-changed=wrapper.h");
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=wrapper.h");
-
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .use_core()
-        .ctypes_prefix("::libc")
-        .blocklist_item(".*mjit.*")
-        .blocklist_item("^FP_.*")
-        .allowlist_file(".*/ruby/[^/]+\\.h")
-        .rustified_enum("*")
-        .new_type_alias_deref("VALUE")
-        .default_alias_style(bindgen::AliasVariation::NewType)
-        .derive_eq(true)
-        .derive_debug(true)
-        .clang_args(&[
-            format!("-I{}", rbconfig("rubyhdrdir")),
-            format!("-I{}", rbconfig("rubyarchhdrdir")),
-            "-fdeclspec".to_string(),
-        ])
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+  // Write the bindings to the $OUT_DIR/bindings.rs file.
+  let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+  bindings
+    .write_to_file(out_path.join("bindings.rs"))
+    .expect("Couldn't write bindings!");
 }
