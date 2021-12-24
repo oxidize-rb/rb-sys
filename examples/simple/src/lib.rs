e3980279a2aa4f8c5d640cfe701bf2430683e2f1
Ian Ker-Seymer <i.kerseymer@gmail.com>
2021-12-24 17:33:49 -0500

    Add an example

---
+++ examples/simple/src/lib.rs
@@ -0,0 +1,30 @@
+extern crate rb_sys;
+
+use rb_sys::*;
+use std::ffi::{CString, CStr};
+use std::os::raw::{c_long};
+
+#[no_mangle]
+unsafe extern "C" fn pub_reverse(_klass: Value, mut input: Value) -> Value {
+  let ruby_string = CStr::from_ptr(rb_string_value_cstr(&mut input))
+    .to_str()
+    .unwrap();
+  let reversed = ruby_string.to_string().chars().rev().collect::<String>();
+  let reversed_cstring = CString::new(reversed).unwrap();
+  let size = ruby_string.len() as c_long;
+
+  rb_utf8_str_new(reversed_cstring.as_ptr(), size)
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn Init_rust_ruby_example() {
+  let name = CString::new("RustRubyExample").unwrap();
+  let function_name = CString::new("reverse").unwrap();
+
+  unsafe {
+    let klass = rb_define_module(name.as_ptr());
+    let callback = std::mem::transmute::<unsafe extern "C" fn(Value, Value) -> Value, unsafe extern "C" fn() -> Value>(pub_reverse);
+    rb_define_module_function(klass, function_name.as_ptr(), Some(callback), 1)
+  }
+}
