eab70bb84653f304ea1183a079716f6c94633f95
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-05-12 21:08:07 -0400

    Init stack

--- crates/rb-sys-tests/tests/helpers.rs
+++ crates/rb-sys-tests/tests/helpers.rs
@@ -4,6 +4,8 @@ static INIT: Once = Once::new();
 
 pub fn setup_ruby_vm() {
     INIT.call_once(|| unsafe {
+        let variable_in_this_stack_frame: rb_sys::VALUE = std::mem::zeroed();
+        rb_sys::ruby_init_stack(variable_in_this_stack_frame as *mut _);
         rb_sys::ruby_init();
     });
 }
