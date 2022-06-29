198f25f96aa74aaf1add43c41ec902cb67d7d7e8
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-06-28 22:44:35 -0400

    Document ruby_abi_version

--- crates/rb-sys/src/ruby_abi_version.rs
+++ crates/rb-sys/src/ruby_abi_version.rs
@@ -13,10 +13,10 @@ pub const __RB_SYS_RUBY_ABI_VERSION: std::os::raw::c_ulonglong = 0;
 #[cfg(has_ruby_abi_version)]
 pub const __RB_SYS_RUBY_ABI_VERSION: std::os::raw::c_ulonglong = crate::RUBY_ABI_VERSION as _;
 
-/// Defines the `ruby_abi_version` function needed for extensions.
 #[macro_export]
 macro_rules! ruby_abi_version {
     () => {
+        /// Defines the `ruby_abi_version` function needed for Ruby extensions.
         #[no_mangle]
         #[allow(unused)]
         pub extern "C" fn ruby_abi_version() -> std::os::raw::c_ulonglong {
