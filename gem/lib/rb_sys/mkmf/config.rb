ddd4c9db80cf81c5d476d38c60c700dde3bbd377
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-07-19 15:30:34 -0400

    Cleanup target dir by default after `gem install` (#67)
    * Cleanup target dir by default after `gem install`
    
    * Bust cache
    
    * Be more specific about what to clean from target dir

--- gem/lib/rb_sys/mkmf/config.rb
+++ gem/lib/rb_sys/mkmf/config.rb
@@ -4,11 +4,12 @@ module RbSys
   module Mkmf
     # Config that delegates to CargoBuilder if needded
     class Config
-      attr_accessor :force_install_rust_toolchain
+      attr_accessor :force_install_rust_toolchain, :clean_after_install
 
       def initialize(builder)
         @builder = builder
         @force_install_rust_toolchain = false
+        @clean_after_install = rubygems_invoked?
       end
 
       def method_missing(name, *args, &blk)
@@ -17,6 +18,15 @@ module RbSys
 
       def respond_to_missing?(name, include_private = false)
         @builder.respond_to?(name) || super
+      end
+
+      private
+
+      # Seems to be the only way to reliably know if we were invoked by Rubygems.
+      # We want to know this so we can cleanup the target directory after an
+      # install, to remove bloat.
+      def rubygems_invoked?
+        ENV.key?("SOURCE_DATE_EPOCH")
       end
     end
   end
