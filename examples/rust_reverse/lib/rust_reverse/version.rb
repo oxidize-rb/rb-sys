f21b76c621497f1039726e24b464ccba526848aa
Ian Ker-Seymer <hello@ianks.com>
2022-07-19 10:22:14 -0400

    Bump rb_sys to 0.9.21

--- examples/rust_reverse/lib/rust_reverse/version.rb
+++ examples/rust_reverse/lib/rust_reverse/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RustReverse
-  VERSION = "0.9.20"
+  VERSION = "0.9.21"
 end
--- gem/lib/rb_sys/version.rb
+++ gem/lib/rb_sys/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RbSys
-  VERSION = "0.9.20"
+  VERSION = "0.9.21"
 end
