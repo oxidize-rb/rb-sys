3e4a6e845aad878164671fdb3daf2ea38ef023af
Ian Ker-Seymer <hello@ianks.com>
2022-07-26 10:57:19 -0400

    Do not attempt to set cargo linker

--- docker/setup/rubybashrc.sh
+++ docker/setup/rubybashrc.sh
@@ -32,7 +32,7 @@ main() {
   set_target_env_for_key_matching "^CC_"
   set_target_env_for_key_matching "^CXX_"
   set_target_env_for_key_matching "^AR_"
-  set_target_env_for_key_matching "^CARGO_TARGET_.*_LINKER"
+  # set_target_env_for_key_matching "^CARGO_TARGET_.*_LINKER"
 
   rm "${0}"
 }
