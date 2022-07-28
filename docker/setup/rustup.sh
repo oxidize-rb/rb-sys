96336d023708a6b3d105d5c6f5da0c2d3123b6c6
Ian Ker-Seymer <hello@ianks.com>
2022-07-28 09:51:24 -0400

    Pre-warm cargo registry

--- docker/setup/rustup.sh
+++ docker/setup/rustup.sh
@@ -14,8 +14,11 @@ main() {
   local url="https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init"
   curl --retry 3 --proto '=https' --tlsv1.2 -sSf "$url" > rustup-init
   chmod +x rustup-init
-  ./rustup-init --no-modify-path --default-toolchain "$RUSTUP_DEFAULT_TOOLCHAIN-$RUST_TARGET" --profile minimal -y
+  ./rustup-init --no-modify-path --default-toolchain "$RUSTUP_DEFAULT_TOOLCHAIN" --profile minimal -y
+  rustup target add "$RUST_TARGET"
   chmod -R a+w "$RUSTUP_HOME" "$CARGO_HOME"
+  cargo init --name tmp .
+  cargo fetch
 
   builtin popd
   rm -rf "${td}"
