901d5b5b39e2b1d5d149c7f7aa8b629e51b699d5
Ian Ker-Seymer <hello@ianks.com>
2022-07-28 08:32:35 -0400

    Install cmake first, set rustup default toolchain in docker

--- docker/setup/rustup.sh
+++ docker/setup/rustup.sh
@@ -14,9 +14,8 @@ main() {
   local url="https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init"
   curl --retry 3 --proto '=https' --tlsv1.2 -sSf "$url" > rustup-init
   chmod +x rustup-init
-  ./rustup-init --no-modify-path --default-toolchain "$RUSTUP_DEFAULT_TOOLCHAIN" --profile minimal -y || true # i686 has an ignorable error
+  ./rustup-init --no-modify-path --default-toolchain "$RUSTUP_DEFAULT_TOOLCHAIN-$RUST_TARGET" --profile minimal -y
   chmod -R a+w "$RUSTUP_HOME" "$CARGO_HOME"
-  rustup target add "$RUST_TARGET"
 
   builtin popd
   rm -rf "${td}"
