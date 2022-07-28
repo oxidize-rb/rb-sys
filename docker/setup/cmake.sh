22602b6148ccb8f1927028cb042bd2e3f058be93
Ian Ker-Seymer <hello@ianks.com>
2022-07-27 22:22:38 -0400

    Set cmake properly

--- docker/setup/cmake.sh
+++ docker/setup/cmake.sh
@@ -19,7 +19,7 @@ main() {
     pushd "${td}"
 
     curl --retry 3 -sSfL "https://github.com/Kitware/CMake/releases/download/v${version}/cmake-${version}-linux-x86_64.sh" -o cmake.sh
-    sh cmake.sh --skip-license --prefix=/usr/local
+    sh cmake.sh --skip-license --prefix=/opt/cmake
 
     popd
 
