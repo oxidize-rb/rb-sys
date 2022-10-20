371d7d3ae075413c31de550ce2f6b4d1965619a8
Ian Ker-Seymer <hello@ianks.com>
2022-10-20 10:13:19 -0400

    Bump to v0.9.33

--- examples/rust_reverse/lib/rust_reverse/version.rb
+++ examples/rust_reverse/lib/rust_reverse/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RustReverse
-  VERSION = "0.9.32"
+  VERSION = "0.9.33"
 end
--- gem/lib/rb_sys/version.rb
+++ gem/lib/rb_sys/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RbSys
-  VERSION = "0.9.32"
+  VERSION = "0.9.33"
 end
