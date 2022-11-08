d206838fa1e5ff5c0732a3348f65726d9df21add
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-11-08 04:34:48 +0000

    Source chruby in devcontainer

--- .devcontainer/on-post-attach.sh
+++ .devcontainer/on-post-attach.sh
@@ -4,6 +4,7 @@ set -ex
 
 # shellcheck disable=SC1091
 [[ -f /etc/rubybashrc ]] && source /etc/rubybashrc
+[[ -f /usr/local/share/chruby/chruby.sh ]] && source /usr/local/share/chruby/chruby.sh
 
 bundle install --jobs 3
 cargo update --dry-run
