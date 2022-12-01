17726f521a5f62538ac490c7b06a9c162f78000c
Ian Ker-Seymer <hello@ianks.com>
2022-12-01 09:58:13 -0500

    Moar debug

--- script/ci/upload-on-failure.sh
+++ script/ci/upload-on-failure.sh
@@ -6,17 +6,26 @@
 #   $ run-and-upload-on-failure.sh "cargo test --all-features" "cargo-test" "target/debug"
 main() {
   if "$1"; then
+    if [[ "${DEBUG:-false}" == "true" ]]; then
+      upload_dir "$2" "$3"
+    fi
+
     exit 0
   else
     exit_code=$?
-    output_dir="${DEBUG_OUTPUT_DIR:-/tmp/debug}/$2"
-    output_file="${output_dir}.tar"
-
-    mkdir -p "$output_dir"
-    tar -cf "$output_file" -C "$3" .
-    echo "[ERROR] Command failed, debug output saved to $output_file" >&2
+    upload_dir "$2" "$3"
     exit $exit_code
   fi
+}
+
+
+upload_dir() {
+  output_dir="${DEBUG_OUTPUT_DIR:-/tmp/debug}/$1"
+  output_file="${output_dir}.tar"
+
+  mkdir -p "$output_dir"
+  tar -cf "$output_file" -C "$2" .
+  echo "[INFO] Debug output saved to $output_file" >&2
 }
 
 main "$@"
