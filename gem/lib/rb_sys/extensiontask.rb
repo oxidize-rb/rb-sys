cee502e0bb228e1e8f1b0ec05d761cfee4041cba
Ian Ker-Seymer <hello@ianks.com>
2023-02-08 22:48:46 -0500

    Lint gemspec in extension task

--- gem/lib/rb_sys/extensiontask.rb
+++ gem/lib/rb_sys/extensiontask.rb
@@ -4,7 +4,7 @@ require_relative "error"
 begin
   require "rake/extensiontask"
 rescue LoadError
-  abort "Please install rake-compiler to use this gem"
+  abort "Please install rake-compiler to use this feature"
 end
 
 module RbSys
@@ -22,8 +22,9 @@ module RbSys
   # @param gem_spec [Gem::Specification] the gem specification to build (needed for cross-compiling)
   # @return [Rake::ExtensionTask]
   class ExtensionTask < Rake::ExtensionTask
-    def init(name = nil, gem_spec = nil)
-      super
+    def init(name = nil, gem_spec = :undefined)
+      super(name, lint_gem_spec(name, gem_spec))
+
       @orginal_ext_dir = @ext_dir
       @ext_dir = cargo_metadata.manifest_directory
       @source_pattern = nil
@@ -117,6 +118,35 @@ module RbSys
 
       desc 'Compile the native Rust extension with the "release" profile'
       task "compile:release" => ["rb_sys:env:release", "compile"]
+    end
+
+    private
+
+    def lint_gem_spec(name, gs)
+      gem_spec = case gs
+      when :undefined
+        return
+      when Gem::Specification
+        gs
+      when String
+        Gem::Specification.load(gem_spec) || raise(ArgumentError, "Unable to load gemspec from file #{gs.inspect}")
+      else
+        raise ArgumentError, "gem_spec must be a Gem::Specification, got #{gs.class}"
+      end
+
+      gem_spec.files.each do |f|
+        if /\.(dll|so|dylib|lib|bundle)$/.match?(f)
+          warn "⚠️ gemspec includes native artifact (#{f}), please remove it."
+        end
+      end
+
+      if (gem_crate_name = gem_spec.metadata["cargo_crate_name"])
+        if name != gem_crate_name
+          warn "⚠️ cargo_crate_name (#{gem_crate_name}) does not match extension task crate name (#{name})"
+        end
+      end
+
+      gem_spec
     end
   end
 end
