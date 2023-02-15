239e3d79febdcd6e27c22bb749e98e714e4c1951
Ian Ker-Seymer <hello@ianks.com>
2023-02-15 14:12:45 -0500

    Use uintptr_t for VALUE and ID

--- crates/rb-sys/src/bindings.rs
+++ crates/rb-sys/src/bindings.rs
@@ -13,7 +13,25 @@
 
 use crate::os::raw::*;
 
-pub type VALUE = ::core::ffi::c_ulong;
-pub type ID = ::core::ffi::c_ulong;
+/// Type that represents a Ruby object.  It is an unsigned integer of some kind,
+/// depending on platforms.
+///
+/// ```c
+/// VALUE value = rb_eval_string("ARGF.readlines.map.with_index");
+/// ```
+///
+/// @warning  ::VALUE is not a pointer.
+/// @warning  ::VALUE can be wider than `long`.
+pub type VALUE = ::libc::uintptr_t;
+
+/// Type that represents a Ruby identifier such as a variable name.
+///
+/// ```CXX
+/// ID method = rb_intern("method");
+/// VALUE result = rb_funcall(obj, method, 0);
+/// ```
+///
+/// @note  ::rb_cSymbol is a Ruby-level data type for the same thing.
+pub type ID = ::libc::uintptr_t;
 
 include!(env!("RB_SYS_BINDINGS_PATH"));
