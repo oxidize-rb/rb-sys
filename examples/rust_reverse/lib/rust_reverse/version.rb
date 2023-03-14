dfc51c60bef89925bfded1713e9ec02c5dfb639d
Ian Ker-Seymer <hello@ianks.com>
2023-03-13 23:09:37 -0400

    Bump to v0.9.68

--- examples/rust_reverse/lib/rust_reverse/version.rb
+++ examples/rust_reverse/lib/rust_reverse/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RustReverse
-  VERSION = "0.9.67"
+  VERSION = "0.9.68"
 end
--- gem/lib/rb_sys/version.rb
+++ gem/lib/rb_sys/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RbSys
-  VERSION = "0.9.67"
+  VERSION = "0.9.68"
 end
