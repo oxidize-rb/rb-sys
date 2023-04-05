bd07c5b5a8dc3a900e324ad387027d037efa8049
Ian Ker-Seymer <hello@ianks.com>
2023-04-05 01:02:28 -0400

    do not explicitly include msys paths

--- crates/rb-sys-build/src/bindings.rs
+++ crates/rb-sys-build/src/bindings.rs
@@ -31,11 +31,11 @@ pub fn generate(
         extension_flag.to_string(),
     ];
 
-    // see https://github.com/oxidize-rb/actions/issues/20
-    if let Ok(_devkit_home) = env::var("RI_DEVKIT") {
-        clang_args.push("-IC:/msys64/mingw64/include".to_string());
-        clang_args.push("-IC:/msys64/ucrt64/include".to_string());
-    }
+    // // see https://github.com/oxidize-rb/actions/issues/20
+    // if let Ok(_devkit_home) = env::var("RI_DEVKIT") {
+    //     clang_args.push("-IC:/msys64/mingw64/include".to_string());
+    //     clang_args.push("-IC:/msys64/ucrt64/include".to_string());
+    // }
 
     clang_args.extend(rbconfig.cflags.clone());
     clang_args.extend(rbconfig.cppflags());
