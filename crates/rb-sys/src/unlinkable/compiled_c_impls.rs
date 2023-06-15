2e1c1734f7d06f8a205a36c7464ec6c8afe7aeac
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2023-06-15 11:38:10 -0400

    Link correctly against RARRAY_LEN compiled C func

--- crates/rb-sys/src/unlinkable/compiled_c_impls.rs
+++ crates/rb-sys/src/unlinkable/compiled_c_impls.rs
@@ -13,7 +13,7 @@ extern "C" {
     #[link_name = "rb_sys_compiled_c_impls_RSTRING_PTR"]
     pub(crate) fn rstring_ptr(str: VALUE) -> *const c_char;
 
-    #[link_name = "rb_sys_compiled_c_impls_RSTRING_LEN"]
+    #[link_name = "rb_sys_compiled_c_impls_RARRAY_LEN"]
     pub(crate) fn rarray_len(ary: VALUE) -> c_long;
 
     #[link_name = "rb_sys_compiled_c_impls_RARRAY_CONST_PTR"]
