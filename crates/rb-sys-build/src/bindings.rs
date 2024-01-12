5205e6d86fc17a37f7c7fdf5cc1b53ff3ccb3921
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2024-01-12 16:31:30 -0500

    Bring comments back

--- crates/rb-sys-build/src/bindings.rs
+++ crates/rb-sys-build/src/bindings.rs
@@ -142,7 +142,7 @@ fn default_bindgen(clang_args: Vec<String>) -> bindgen::Builder {
         .blocklist_item("^rb_native.*")
         .opaque_type("^__sFILE$")
         .merge_extern_blocks(true)
-        .generate_comments(false)
+        .generate_comments(true)
         .size_t_is_usize(env::var("CARGO_FEATURE_BINDGEN_SIZE_T_IS_USIZE").is_ok())
         .impl_debug(cfg!(feature = "bindgen-impl-debug"))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()));
