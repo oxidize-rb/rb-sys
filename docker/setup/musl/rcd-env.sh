3e91b96bd1828450915c1eb7018b9d413a661f2b
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2024-04-18 17:40:12 -0400

    Remove dead musl code

--- docker/setup/musl/rcd-env.sh
+++
@@ -1,6 +0,0 @@
-# set up a working RCD build environment
-export RAKE_EXTENSION_TASK_NO_NATIVE=true
-if ! test -e "$HOME"/.rake-compiler ; then
-  ln -s /usr/local/rake-compiler "$HOME"/.rake-compiler
-fi
-mkdir -p "$HOME"/.gem
