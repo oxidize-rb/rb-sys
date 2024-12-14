beaf6805ecf4172520b45f8ab13b94a37d1e6a04
Ian Ker-Seymer <i.kerseymer@gmail.com>
2024-12-14 18:58:25 -0500

    Various updates for docker images (#466)

--- docker/setup/rubygems.sh
+++ docker/setup/rubygems.sh
@@ -7,8 +7,8 @@ set -euo pipefail
 source /lib.sh
 
 main() {
-  rubygems_version="3.4.4"
-  bundler_version="2.4.4"
+  rubygems_version="3.5.23"
+  bundler_version="2.5.23"
 
   gem update --no-document --system $rubygems_version
   gem install bundler:$bundler_version --no-document
