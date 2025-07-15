b46c03233dfbd70037f79a72428616b2b5f19d72
ianks <ianks@users.noreply.github.com>
2025-07-15 04:12:22 +0000

    deploy: 8fdabbd37d51bf8eff52c3cf08285adfe4e8e3cc

--- hello_rusty/lib/hello_rusty.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "hello_rusty/version"
-require_relative "hello_rusty/hello_rusty"
-
-module HelloRusty
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- hello_rusty/lib/hello_rusty/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module HelloRusty
-  VERSION = "0.1.0"
-end
